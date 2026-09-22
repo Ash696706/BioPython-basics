bb1d303a7d268e723ac8dbbc0b7c6349ae7fd7a6
Akshara <777aksharasharma@gmail.com>
2026-09-22 19:17:03 +0530

    Implement most frequent nucleotide detection
    Added functionality to identify the most frequent nucleotide in the DNA sequence.

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -9,3 +9,13 @@ print(g)
 print(c)
 gc_content = (g + c) / len(dna) * 100
 print("GC content:",round(gc_content),2, "%")
+highest = max(a,t,g,c)
+print(highest)
+if highest == a:
+  print("A is most frequent nucleotide")
+elif highest == b:
+  print("B is most frequent nucleotide")
+elif highest == c:
+  print("C is most frequent nucleotide")
+else:
+  print("G is most frequent nucleotide")
