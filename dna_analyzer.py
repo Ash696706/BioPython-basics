a390f01f3931c7286404c20cf95c0a30bc19c51e
Akshara <777aksharasharma@gmail.com>
2026-09-22 19:35:09 +0530

    Refactor DNA analysis and fix output statements
    Updated nucleotide frequency analysis and output formatting.

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -1,5 +1,6 @@
-#GC content
+#DNA sequence
 dna = "ATGCCATCCGATCGATTACGGGA"
+#count ATGC
 a = dna.count("A")
 t = dna.count("T")
 g = dna.count("G")
@@ -8,6 +9,7 @@ print(a)
 print(t)
 print(g)
 print(c)
+#GC content
 gc_content = (g + c) / len(dna) * 100
 print("GC content:",round(gc_content),2, "%")
 # maximum value
@@ -15,29 +17,29 @@ highest = max(a,t,g,c)
 print(highest)
 if highest == a:
   print("A is most frequent nucleotide")
-elif highest == b:
-  print("B is most frequent nucleotide")
-elif highest == c:
-  print("C is most frequent nucleotide")
-else:
+elif highest == t:
+  print("T is most frequent nucleotide")
+elif highest == g:
   print("G is most frequent nucleotide")
+else:
+  print("C is most frequent nucleotide")
 # minimum value   
 minimum = min(a,t,g,c)
 print(minimum)
 if minimum == a:
-  print(A)
+  print("A")
 elif minimum == g:
-  print(G)
+  print("G")
 elif minimum == c:
-  print(C)
+  print("C")
 else:
-  print(T)
+  print("T")
 #count nucleotide percentage
 percentage_a = a/len(dna)*100
-print("percentage of A:",round(percentage_a,2),"%")
+print("percentage of A:",round(percentage_a, 2), "%")
 percentage_t = t/len(dna)*100
-print("percentage of T:",round(percentage_t,2),"%")
+print("percentage of T:",round(percentage_t, 2), "%")
 percentage_g = g/len(dna)*100
-print("percentage of G:",round(percentage_g,2),"%")
+print("percentage of G:",round(percentage_g, 2), "%")
 percentage_c = c/len(dna)*100
-print("percentage of C:",round(percentage_c,2),"%")
+print("percentage of C:",round(percentage_c, 2), "%")
